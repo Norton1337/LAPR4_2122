5fe5f95769cf4092b9dc9e96894bfa14e9b64958
N0R70N <1210824@isep.ipp.pt>
2022-06-07 15:52:02 +0100

    Fixed .bat files

--- base.core/src/main/java/eapli/base/productmanagement/application/SpecifyProductController.java
+++
@@ -1,9 +0,0 @@
-package eapli.base.productmanagement.application;
-
-public class SpecifyProductController {
-
-    public void specifyProduct(String productCode, String barCode, String reference, String brandName,
-                               String shortDescription, String technicalDescription, String extendedDescription){
-
-    }
-}
