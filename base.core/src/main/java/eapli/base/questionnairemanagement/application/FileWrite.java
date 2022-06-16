23385f320c20ab7cafed43cbdc86db8084883f9c
André Maia <1191259@isep.ipp.pt>
2022-06-16 21:34:44 +0100

    Controller and FileWriter update

--- base.core/src/main/java/eapli/base/questionnairemanagement/application/FileWrite.java
+++ base.core/src/main/java/eapli/base/questionnairemanagement/application/FileWrite.java
@@ -10,7 +10,7 @@ public class FileWrite {
         try {
             BufferedWriter buffWrite = new BufferedWriter(f);
             buffWrite.append(title + "\n\n");
-            buffWrite.append(wmsg + "\n\n");
+            buffWrite.append(wmsg + "\n");
 
             buffWrite.close();
         } catch (IOException e) {
@@ -18,17 +18,41 @@ public class FileWrite {
         }
     }
 
-
-    public void addQuestion(String text, Integer type, Integer atype, String fileName) throws IOException {
+   // Section2: Other (this section is about other stuff)
+    public void addSection(String text, String fileName, Integer number, String s) throws IOException {
         // Abrir ficheiro ja criado e adiciona texto
-
         Writer buffWrite = new FileWriter(fileName, true);
-        buffWrite.append("Q. " +text+" ("+type+")["+atype+"]\n");
+        buffWrite.append("\n");
+        buffWrite.append("\n Section"+number+": " +text+" ("+s+")");
         buffWrite.close();
     }
 
 
-    public void addPossibleAnswer(String answer, String fileName) {
+    public void addQuestion(String text, Integer type, Integer atype, String fileName, Integer number) throws IOException {
+        // Abrir ficheiro ja criado e adiciona texto
+        String s="";
+        String s1="";
+        Writer buffWrite = new FileWriter(fileName, true);
+            if(type==1)
+                s = "optional";
+            if(type==2)
+                s = "obligatory";
+            if(atype==1)
+                s1 = "Multiple Choice";
+                if(atype==2)
+                    s1 ="Numeric";
+                    if (atype==3)
+                        s1 ="Free Text";
+        buffWrite.append("\n");
+        buffWrite.append("\n");
+        buffWrite.append("\nQ"+number+". " +text+" ("+s+")["+s1+"]");
+        buffWrite.close();
+    }
 
+
+    public void addPossibleAnswer(String answer, String fileName) throws IOException {
+        Writer buffWrite = new FileWriter(fileName, true);
+        buffWrite.append(" "+answer+" |");
+        buffWrite.close();
     }
 }
--- base.core/src/main/java/eapli/base/questionnairemanagement/application/QuestionnaireController.java
+++ base.core/src/main/java/eapli/base/questionnairemanagement/application/QuestionnaireController.java
@@ -1,46 +1,48 @@
 package eapli.base.questionnairemanagement.application;
 
 
+import eapli.base.ordermanagement.domain.OrderType;
+import eapli.base.productmanagement.repositories.ProductRepository;
+import eapli.base.questionnairemanagement.QuestionnaireRepository;
+import eapli.base.questionnairemanagement.domain.Questionnaire.FileName;
+import eapli.base.questionnairemanagement.domain.Questionnaire.QuestionnaireTxt;
+import eapli.base.taskmanagement.domain.Task;
+import eapli.base.taskmanagement.domain.TaskDateTime;
+import eapli.base.taskmanagement.domain.TaskID;
+import eapli.base.taskmanagement.repositories.TaskRepository;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 public class QuestionnaireController {
 
-        public void openFile(String file) throws FileNotFoundException {
-
-            try {
-                String line;
-                if (file.equals("questionarioTeste.txt")){
-                    BufferedReader br = new BufferedReader(new FileReader("lprog/userstory/src/Questionarios/questionarioTeste.txt"));
-                    while ((line = br.readLine()) != null) {
-                        System.out.println(line);
-                    }
-                    br.close();
-                }
-
-
-            }catch (IOException e){
-                e.printStackTrace();
-            }
-
-        }
-
+    private final QuestionnaireRepository questionnaireRepository = eapli.base.infrastructure.persistence.PersistenceContext.repositories().questionnaire();
+    private FileName fileName;
     FileWrite filew = new FileWrite();
 
         public void createFile(String fileName, String title, String wmsg) throws IOException {
         // enviar para makefile estes dados,criar ficheiro e abrir
             filew.createFile(fileName, title, wmsg);
+            //QuestionnaireTxt novo =
+            //questionnaireRepository.save();
+
         }
 
-    public void addQuestion(String text, Integer type, Integer atype, String fileName) throws IOException {
+    public void addSection(String text, String fileName, Integer number, String s) throws IOException {
         // enviar para makefile os dados da questao, utilizar ficheiro ja criado com nome fileName
-        filew.addQuestion(text,type,atype,fileName);
+        filew.addSection(text,fileName, number,s);
+    }
+
+    public void addQuestion(String text, Integer type, Integer atype, String fileName, Integer number) throws IOException {
+        // enviar para makefile os dados da questao, utilizar ficheiro ja criado com nome fileName
+        filew.addQuestion(text,type,atype,fileName, number);
     }
 
 
-    public void addPossibleAnswer(String answer, String fileName) {
+    public void addPossibleAnswer(String answer, String fileName) throws IOException {
 
         // enviar para makefile os dados das respostas, utilizar ficheiro ja criado com nome fileName
         // vai ser adicionado em frente á addQuestion() , ver formataçao de txt com o paulo por causa da grammar
