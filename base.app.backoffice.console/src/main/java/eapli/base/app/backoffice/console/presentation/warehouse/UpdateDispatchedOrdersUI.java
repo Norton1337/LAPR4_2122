33f54155474727649eff32f561c16cdaadced379
Ana  Soares <1210802@isep.ipp.pt>
2022-06-18 18:15:06 +0100

    US1006 DispatchedOrdersUI development

--- base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/warehouse/UpdateDispatchedOrdersUI.java
+++ base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/warehouse/UpdateDispatchedOrdersUI.java
@@ -1,10 +1,68 @@
 package eapli.base.app.backoffice.console.presentation.warehouse;
 
 import eapli.base.ordermanagement.application.OrderController;
+import eapli.base.ordermanagement.domain.OrderType;
+import eapli.base.taskmanagement.domain.Task;
+import eapli.base.warehouses.domain.agvs.AGV;
+import eapli.framework.io.util.Console;
+
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UpdateDispatchedOrdersUI {
 
     private final OrderController orderController = new OrderController();
+
+
+    public boolean show(){
+        System.out.println("");
+
+        int i =0;
+
+        List<OrderType> dispatchedOrdersList = orderController.getDispatchedOrders();
+        List<OrderType> updatedOrders = new ArrayList<>();
+
+        if (dispatchedOrdersList.isEmpty()){
+            System.out.println("There are no dispatched orders");
+            return false;
+        }
+
+        System.out.println("Would you like to change order status to 'being delivered' ?");
+
+        int option = Console.readInteger("0-No\n1-Yes\n>");
+        if(option != 1){
+            System.out.println("Invalid option");
+            return false;
+        }
+
+        boolean flag = true;
+        while (flag){
+            for (OrderType order : dispatchedOrdersList){
+
+                System.out.println("["+i+"] "+order.toString());
+                i++;
+            }
+            System.out.println("Choose order you would like to update");
+            int orderToUp = Console.readInteger("-->");
+            OrderType orderToUpdate = dispatchedOrdersList.get(orderToUp); //starts at 0
+            if (!updatedOrders.contains(orderToUpdate)){
+
+                //------- UPDATE ORDER TO BEING DELIVERED  ------
+                updatedOrders.add(orderToUpdate);
+            }
+            System.out.println("Would you like to update more orders?");
+            int upd = Console.readInteger("0-No\n1-Yes\n>");
+            if (upd == 0){
+                flag = false;
+            }
+
+        }
+
+
+
+
+        return false;
+    }
 }
--- base.core/src/main/java/eapli/base/ordermanagement/application/OrderController.java
+++ base.core/src/main/java/eapli/base/ordermanagement/application/OrderController.java
@@ -57,6 +57,11 @@ public class OrderController {
        return orderRepository.findOrdersByTime();
     }
 
+    public List<OrderType> getDispatchedOrders(){
+        return orderRepository.findDispatchedOrders();
+    }
+
+
     public void createOrder(List<CartItem> cartItems, String billingAddress, String postalAddress, SystemUser systemUser){
         List<OrderItem> orderItemList = new ArrayList<>();
         Double totalPrice = 0.0;
