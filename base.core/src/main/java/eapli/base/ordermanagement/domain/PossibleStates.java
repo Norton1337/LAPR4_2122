fe4daccca470f4ccd27496455c774a54f9d2c5b0
Ana  Soares <1210802@isep.ipp.pt>
2022-06-18 16:09:14 +0100

    US1006 Adding new state "being delivered" to orders

--- base.core/src/main/java/eapli/base/ordermanagement/domain/PossibleStates.java
+++ base.core/src/main/java/eapli/base/ordermanagement/domain/PossibleStates.java
@@ -1,6 +1,6 @@
 package eapli.base.ordermanagement.domain;
 
 public enum PossibleStates {
-        WAITING, IN_PROGRESS, COMPLETED, DISPATCHED, CLOSED;
+        WAITING, IN_PROGRESS, COMPLETED, DISPATCHED, BEING_DELIVERED, CLOSED;
 
 }
